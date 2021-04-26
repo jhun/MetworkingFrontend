b232df6cf2e06c70971719642f51b5b0e554bd66
jhunkusano-umantech <jhun.kusano@umantech.com.br>
2021-04-26 08:56:40 -0300

    - Axios with the new url connections

--- src/Services/Axios/index.js
+++ src/Services/Axios/index.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://metworking-env.eba-ueztaznm.us-east-1.elasticbeanstalk.com",
+  baseURL:
+    "http://metworking-env.eba-drmcderk.us-east-1.elasticbeanstalk.com:81",
 });
 
 export const apiMatch = axios.create({
   baseURL:
-    "http://metworking-env.eba-ueztaznm.us-east-1.elasticbeanstalk.com:81",
+    "http://metworking-env.eba-drmcderk.us-east-1.elasticbeanstalk.com:82",
 });
 
 export const apiGeo = axios.create({
   baseURL:
-    "http://metworking-env.eba-ueztaznm.us-east-1.elasticbeanstalk.com:82",
+    "http://metworking-env.eba-drmcderk.us-east-1.elasticbeanstalk.com:83",
 });
 
 export default api;
